02393d125dde36b6d9ba5130ddfff280bbff38af
Anuraga Sahoo <105196471+Anuraga-Sahoo@users.noreply.github.com>
2024-02-20 21:07:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// --------home Typed Text -----------
+var typed = new Typed('.text', {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
+  // ----------
